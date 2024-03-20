0264772508ef24b2677bc47d8e1bc8a16b16dcf1
jamesw732 <jamesmw732@gmail.com>
2024-03-19 20:52:21 -0400

    Split player into own file, full range of motion, set min/max camera distance

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
 from ursina import *
-import math
-# create a window
+from player import *
+
 app = Ursina()
-
-class Player(Entity):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.speed = 5
-        self.camdistance = 100
-        self.focus = Entity(model="cube", scale=0.1, position=self.position + Vec3(0, 0.8 * self.scale_y, 0))
-        camera.parent = self.focus
-        camera.position = (0, 0, -1 * self.camdistance)
-
-    def update(self):
-        movement_delta = Vec3(held_keys['e'] - held_keys['q'], 0, held_keys['w'] - held_keys['s']).normalized()
-        movement_delta *= time.dt * 5
-        self.position += movement_delta
-        self.focus.position += movement_delta
-        updown_rotation = held_keys["up arrow"] - held_keys["down arrow"]
-        leftright_rotation = held_keys['d'] - held_keys['a']
-        self.focus.rotate((0, leftright_rotation * math.cos(math.radians(self.focus.rotation_x)), 0))
-        if abs(self.focus.rotation_x + updown_rotation) < 89:
-            self.focus.rotate((updown_rotation, 0, 0))
-        self.focus.rotation_z = 0
-        self.rotation_y = self.focus.rotation_y
-        camera.position = (0, 0, -1 * self.camdistance)
-
-    def input(self, key):
-        if key == "scroll up":
-            self.camdistance += 10
-        if key == "scroll down":
-            self.camdistance -= 10
 
 player = Player(model='cube', color=color.orange, scale_y=2, collider="box", origin=(0, -.5, 0))
 ground = Entity(model = "plane", texture="grass", scale = 200, collider="box")
---
+++ player.py
@@ -0,0 +1,42 @@
+from ursina import *
+import numpy
+
+
+class Player(Entity):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.speed = 5
+        self.camdistance = 200
+        self.focus = Entity(model="cube", scale=0.1, position=self.position + Vec3(0, 0.8 * self.scale_y, 0))
+        camera.parent = self.focus
+        camera.position = (0, 0, -1 * self.camdistance)
+
+    def update(self):
+        self.move()
+        self.rotate_camera()
+
+    def input(self, key):
+        if key == "scroll up":
+            self.camdistance = min(self.camdistance + 10, 1000)
+        if key == "scroll down":
+            self.camdistance = max(self.camdistance - 10, 0)
+
+    def move(self):
+        movement_inputs = Vec3(held_keys['e'] - held_keys['q'], 0, held_keys['w'] - held_keys['s']).normalized()
+        theta = numpy.radians(-1 * self.rotation_y)
+        rotation_matrix = numpy.array([[numpy.cos(theta), -1 * numpy.sin(theta)], [numpy.sin(theta), numpy.cos(theta)]])
+        move_direction = rotation_matrix @ numpy.array([movement_inputs[0], movement_inputs[2]])
+        movement_delta = Vec3(move_direction[0], 0, move_direction[1]) * time.dt * self.speed
+
+        self.position += movement_delta
+        self.focus.position += movement_delta
+
+    def rotate_camera(self):
+        updown_rotation = held_keys["up arrow"] - held_keys["down arrow"]
+        leftright_rotation = held_keys['d'] - held_keys['a']
+        self.focus.rotate((0, leftright_rotation * numpy.cos(numpy.radians(self.focus.rotation_x)), 0))
+        if abs(self.focus.rotation_x + updown_rotation) < 89:
+            self.focus.rotate((updown_rotation, 0, 0))
+        self.focus.rotation_z = 0
+        self.rotation_y = self.focus.rotation_y
+        camera.position = (0, 0, -1 * self.camdistance)
