ee1b20a55ee122a435f22996dedc26c16691b233
jamesw732 <jamesmw732@gmail.com>
2024-04-01 17:26:52 -0400

    Added slightly buggy npc names

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from npc import *
 from combat import *
 from world_gen import *
 
-# def make_npc_names(npcs):
-    # return [Text(npc.name, scale=20, origin=Vec3(0, 0, 0), position=npc.position + Vec3(0, npc.scale_y, 0)) for npc in npcs]
-    # return [Text(npc.name, scale=20, parent=npc, origin=Vec3(0, 0, 0), position=Vec3(0, npc.scale_y, 0)) for npc in npcs]
-
 app = Ursina(borderless=False)
 GenerateWorld("zones/demo.json")
 
@@ -15,17 +11,12 @@ GenerateWorld("zones/demo.json")
 player = Player("Player", model='cube', color=color.orange, scale_y=2, collider="box", origin=(0, -0.5, 0), position=(0, 1, 0))
 npc = NPC("NPC", model='cube', color=color.red, collider="box", scale_y=2, origin=(0, -0.5, 0), position=(10, 0, 10))
 
-# npclabels = make_npc_names([npc])
-# t = Text("Hello World!", scale=2, origin=Vec3(0, 0, 0), position=npc.position)
-
 
 moblist = [player.mob, npc.mob]
 cb = Combat(moblist)
 
 def update():
     cb.main_combat_loop()
-    # t.world_position = Vec3(0, 0, 20)
-    # for label in npclabels:
-    #     label.look_at(player)
+    npc.rotate_namelabel(camera.world_position)
 
 app.run()
--- npc.py
+++ npc.py
@@ -7,7 +7,18 @@ class NPC(Entity):
     def __init__(self, name, *args, mob=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = name
+        self.height = self.scale_y
         if mob:
             self.mob = mob
         else:
             self.mob = Mob(self)
+        self.namelabel = self.make_name_text()
+    
+    def make_name_text(self):
+        return Text(self.name, parent=scene, scale=10, origin=(0, 0, 0), position=self.position + Vec3(0, self.height + 1, 0))
+
+    def rotate_namelabel(self, pos):
+        self.namelabel.look_at(pos)
+        self.namelabel.rotation_x *= -1
+        self.namelabel.rotation_y += 180
+        self.namelabel.rotation_z = 0
