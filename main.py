fdd47f43c9b4fe06e59548f2391e84c11641c3f8
jamesw732 <jamesmw732@gmail.com>
2024-04-01 17:53:35 -0400

    Added npcs to json

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from combat import *
 from world_gen import *
 
 app = Ursina(borderless=False)
-GenerateWorld("zones/demo.json")
+world = GenerateWorld("zones/demo.json")
 
 
 player = Player("Player", model='cube', color=color.orange, scale_y=2, collider="box", origin=(0, -0.5, 0), position=(0, 1, 0))
-npc = NPC("NPC", model='cube', color=color.red, collider="box", scale_y=2, origin=(0, -0.5, 0), position=(10, 0, 10))
 
-
-moblist = [player.mob, npc.mob]
+npcs = world.create_npcs("zones/demo_npcs.json")
+moblist = [player.mob] + [npc.mob for npc in npcs]
 cb = Combat(moblist)
 
 def update():
     cb.main_combat_loop()
-    npc.rotate_namelabel(camera.world_position)
+    for npc in npcs:
+        npc.rotate_namelabel(camera.world_position)
 
 app.run()
--- world_gen.py
+++ world_gen.py
@@ -1,6 +1,9 @@
 # Handles all world generation.
 from ursina import *
+from npc import *
 import json
+
+tuple_vars = ["origin", "position", "world_position", "rotation", "world_rotation", "texture_scale"]
 
 class GenerateWorld:
     def __init__(self, file):
@@ -8,17 +11,23 @@ class GenerateWorld:
             self.parse_json(file)
 
     def parse_json(self, file):
-        tuple_vars = ["origin", "position", "world_position", "rotation", "world_rotation"]
         with open(file) as f:
             world_data = json.load(f)
         for (entity, data) in world_data.items():
             if entity == "Sky":
                 Sky(**data)
             else:
-                data = world_data[entity]
-                if "color" in data:
-                    data["color"] = color.colors[data["color"]]
-                for var in tuple_vars:
-                    if var in data:
-                        data[var] = [float(a) for a in data[var].split(", ")]
-                Entity(**data)
+                Entity(**self.parse_colors_tuples(data))
+    
+    def create_npcs(self, file):
+        with open(file) as f:
+            npc_data = json.load(f)
+        return [NPC(npc, **self.parse_colors_tuples(data)) for (npc, data) in npc_data.items()]
+
+    def parse_colors_tuples(self, data):
+        if "color" in data:
+            data["color"] = color.colors[data["color"]]
+        for var in tuple_vars:
+            if var in data:
+                data[var] = [float(a) for a in data[var].split(", ")]
+        return data
