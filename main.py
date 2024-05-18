3ac46d5d1b15743a75adc5ff1bc9ba29902f340b
jamesw732 <jamesmw732@gmail.com>
2024-05-18 01:54:51 -0400

    Fixed namelabel

--- main.py
+++ main.py
@@ -17,5 +17,11 @@ npcs = world.create_npcs("zones/demo_npcs.json")
 npc_controllers = [NPC_Controller(npc) for npc in npcs]
 moblist = [player.mob] + [npc.mob for npc in npcs]
 
+def update():
+    # Continuous processes local to client
+    for npc in npc_controllers:
+        if npc.namelabel:
+            npc.rotate_namelabel(player.world_position - camera.world_position)
+
 
 app.run()
--- npc_controller.py
+++ npc_controller.py
@@ -8,15 +8,12 @@ class NPC_Controller(Entity):
         super().__init__()
         self.character = character
         self.namelabel = self.make_name_text()
-    
-    def update(self):
-        self.rotate_namelabel(camera.world_position)
 
     def make_name_text(self):
-        return Text(self.name, parent=scene, scale=10, origin=(0, 0, 0), position=self.character.position + Vec3(0, self.character.height + 1, 0))
+        return Text(self.character.name, parent=scene, scale=10, origin=(0, 0, 0), position=self.character.position + Vec3(0, self.character.height + 1, 0))
 
-    def rotate_namelabel(self, pos):
-        self.namelabel.look_at(pos)
-        self.namelabel.rotation_x *= -1
-        self.namelabel.rotation_y += 180
+    def rotate_namelabel(self, direction):
+        """Rotate namelabel to be parallel with player and camera.
+        dir: player world position - camera world position"""
+        self.namelabel.look_at(direction + self.namelabel.world_position)
         self.namelabel.rotation_z = 0
