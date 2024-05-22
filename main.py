c85619b0a3e3807318830476b34db494eb14c7d3
jamesw732 <jamesmw732@gmail.com>
2024-05-22 16:05:43 -0400

    Created data directory

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from npc_controller import *
 
 
 app = Ursina()
-world = GenerateWorld("zones/demo.json")
+world = GenerateWorld("data/zones/demo.json")
 
 
 player = Character("Player", model='cube', color=color.orange, scale_y=2, collider="box", origin=(0, -0.5, 0), position=(0, 1, 0))
 pc = PlayerController(player)
 
 
-npcs = world.create_npcs("zones/demo_npcs.json")
+npcs = world.create_npcs("data/zones/demo_npcs.json")
 npc_controllers = [NPC_Controller(npc) for npc in npcs]
 
 def update():
