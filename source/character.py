eeb4da867d67871dfb33aab4b17b4927979a62ba
jamesw732 <jamesmw732@gmail.com>
2024-06-20 19:17:14 -0400

    Pulled out physics into own file

--- source/character.py
+++ source/character.py
@@ -5,6 +5,7 @@ import numpy
 
 from .combat import *
 from .networking.base import *
+from .physics import handle_movement
 from .world_defns import *
 
 # Update this to expand CharacterState
@@ -24,8 +25,8 @@ char_state_attrs = {
 
 
 class Character(Entity):
-    def __init__(self, *args, name="Player", speed=10.0,  uuid=None, type="player",
-                state=None, **kwargs):
+    def __init__(self, *args, name="Player", speed=10.0,  uuid=None,
+                 type="player", state=None, **kwargs):
         # Engine-relevant vars
         super().__init__(*args, **kwargs)
         if state:
@@ -39,7 +40,6 @@ class Character(Entity):
         self.height = self.scale_y
         self.namelabel = self.make_namelabel()
 
-        self.velocity = Vec3(0, 0, 0)
         self.velocity_components = {}
 
         self.jumping = False
@@ -76,7 +76,7 @@ class Character(Entity):
 
     def update(self):
         if not self.lerping:
-            self.handle_movement()
+            handle_movement(self)
 
         elif self.lerping and self.prev_state:
             self.lerp_timer += time.dt
@@ -104,89 +104,6 @@ class Character(Entity):
         if self.health <= 0:
             if is_main_client():
                 self.die()
-
-    def handle_movement(self):
-        """Combines all movement inputs into one velocity vector, then handles collision and grounding and moves in just one call."""
-        # Get the character-only velocity items
-        self.set_gravity_vel()
-        self.set_jump_vel()
-        # Combine them all, there will likely be others besides gravity and jumping
-        self.velocity = sum(list(self.velocity_components.values()))
-        # If components was empty, we get an int, so convert to zero vector
-        if type(self.velocity) is int:
-            self.velocity = Vec3(0, 0, 0)
-
-        self.handle_grounding()
-        self.handle_collision()
-        self.handle_upward_collision()
-
-        self.position += self.velocity * time.dt
-
-    def set_gravity_vel(self):
-        """If not grounded and not jumping, subtract y (linear in time) from velocity vector"""
-        if not self.grounded and not self.jumping:
-            self.grav -= 1
-        elif self.grounded:
-            self.grav = 0
-        self.velocity_components["gravity"] = Vec3(0, self.grav, 0)
-
-    def set_jump_vel(self):
-        """If jumping, add some y to velocity vector"""
-        if self.jumping:
-            speed = self.max_jump_height / self.max_jump_time
-            if self.rem_jump_height - speed * time.dt <= 0:
-                speed = self.rem_jump_height / self.max_jump_time
-                self.cancel_jump()
-            else:
-                self.rem_jump_height -= speed * time.dt
-            jump_vel = Vec3(0, speed, 0)
-        else:
-            jump_vel = Vec3(0, 0, 0)
-        self.velocity_components["jump"] = jump_vel
-
-    def handle_grounding(self):
-        """Handles logic for whether the character is grounded or not.
-        Doesn't move the character at all, just handles logic for whether or not the player is grounded"""
-        if self.velocity[1] > 0:
-            self.grounded = False
-        elif self.velocity[1] == 0:
-            ground = raycast(self.position, direction=(0, -1, 0), distance=0.01, ignore=self.ignore_traverse)
-            self.grounded = ground.hit
-        else:
-            ground = raycast(self.position, direction=(0, -1, 0), distance=abs(self.velocity[1] * time.dt), ignore=self.ignore_traverse)
-            if ground.hit:
-                self.grounded = True
-
-    def handle_collision(self):
-        """Handles most of the collision logic."""
-        dist = distance((0, 0, 0), self.velocity) * time.dt
-        collision_check = raycast(self.position, direction=self.velocity, distance=dist, ignore=self.ignore_traverse)
-        if collision_check.hit:
-            normal = collision_check.world_normal
-            if numpy.dot(normal, self.velocity) < 0:
-                # Note this is different from character speed if multiple velocity components
-                speed = distance(Vec3.zero, self.velocity)
-                # Project velocity onto normal of colliding entity
-                # This is not correct.
-                self.velocity = self.velocity - (numpy.dot(normal, self.velocity)) * normal
-                # Match old speed
-                self.velocity = self.velocity.normalized() * speed
-                # Make sure new velocity doesn't pass through any entities, ie concave case.
-                # For now, we just don't move at all, but eventually can improve this.
-                dist2 = distance((0, 0, 0), self.velocity) * time.dt
-                ray = raycast(self.position, direction=self.velocity, distance=dist2, ignore=self.ignore_traverse)
-                if ray.hit:
-                    self.velocity = Vec3(0, 0, 0)
-
-    def handle_upward_collision(self):
-        """Blocks upward movement when jumping into a ceiling"""
-        if self.velocity[1] < 0:
-            return
-        # Cast ray from top of model, rather than bottom like in handle_collision
-        pos = self.position + Vec3(0, self.height, 0)
-        ceiling = raycast(pos, direction=(0, 1, 0), distance=self.velocity[1] * time.dt, ignore=self.ignore_traverse)
-        if ceiling.hit:
-            self.velocity[1] = 0
 
     def start_jump(self):
         """Set self.jumping"""
---
+++ source/physics.py
@@ -0,0 +1,98 @@
+from ursina import *
+import numpy
+
+from .world_defns import *
+
+
+def handle_movement(char):
+    """Main physics method which combines all character velocities into one
+    vector, then handles collision and grounding and updates position"""
+    set_gravity_vel(char)
+    set_jump_vel(char)
+
+    velocity = sum(list(char.velocity_components.values()))
+
+    if type(velocity) is int:
+        velocity = Vec3(0, 0, 0)
+
+    handle_grounding(char, velocity)
+    velocity = handle_collision(char, velocity)
+    velocity = handle_upward_collision(char, velocity)
+
+    char.position += velocity * time.dt
+
+
+def set_gravity_vel(char):
+    """If not grounded and not jumping, subtract y (linear in time) from velocity vector"""
+    grav = char.velocity_components.get("gravity", Vec3(0, 0, 0))
+    if not char.grounded and not char.jumping:
+        grav -= Vec3(0, 100 * time.dt, 0)
+    elif char.grounded:
+        grav = Vec3(0, 0, 0)
+    char.velocity_components["gravity"] = grav # Try deleting this line
+
+
+def set_jump_vel(char):
+    """If jumping add, some y to jump velocity vector"""
+    # Meant to simulate the actual act of jumping, ie feet still touching ground
+    if char.jumping:
+        speed = char.max_jump_height / char.max_jump_time
+        if char.rem_jump_height - speed * time.dt <= 0:
+            speed = char.rem_jump_height / char.max_jump_time
+            char.cancel_jump()
+        else:
+            char.rem_jump_height -= speed * time.dt
+        jump_vel = Vec3(0, speed, 0)
+    else:
+        jump_vel = Vec3(0, 0, 0)
+    char.velocity_components["jump"] = jump_vel
+
+
+def handle_grounding(char, velocity):
+    if velocity[1] > 0:
+        char.grounded = False
+    elif velocity[1] == 0:
+        ground = raycast(char.position, direction=(0, -1, 0), distance=0.01, ignore=char.ignore_traverse)
+        char.grounded = ground.hit
+    else:
+        ground = raycast(char.position, direction=(0, -1, 0),
+                         distance=abs(velocity[1] * time.dt),
+                         ignore=char.ignore_traverse)
+        if ground.hit:
+            char.grounded = True
+
+
+def handle_collision(char, velocity):
+    """Handles feet collision logic"""
+    dist = distance((0, 0, 0), velocity) * time.dt
+    collision_check = raycast(char.position, direction=velocity, distance=dist, ignore=char.ignore_traverse)
+    if collision_check.hit:
+        normal = collision_check.world_normal
+        if numpy.dot(normal, velocity) < 0:
+            # Note this is different from character speed if multiple velocity components
+            speed = distance(Vec3.zero, velocity)
+            # Project velocity onto normal of colliding entity
+            # This is not correct.
+            velocity = velocity - (numpy.dot(normal, velocity)) * normal
+            # Match old speed
+            velocity = velocity.normalized() * speed
+            # Make sure new velocity doesn't pass through any entities, ie concave case.
+            # For now, we just don't move at all, but eventually can improve this.
+            dist2 = distance((0, 0, 0), velocity) * time.dt
+            ray = raycast(char.position, direction=velocity, distance=dist2, ignore=char.ignore_traverse)
+            if ray.hit:
+                velocity = Vec3(0, 0, 0)
+    return velocity
+
+
+def handle_upward_collision(char, velocity):
+    """Blocks upward movement when jumping into a ceiling"""
+    if velocity[1] < 0:
+        return velocity
+    # Cast ray from top of model, rather than bottom like in handle_collision
+    pos = char.position + Vec3(0, char.height, 0)
+    ceiling = raycast(pos, direction=(0, 1, 0), distance=velocity[1] * time.dt,
+                      ignore=char.ignore_traverse)
+    if ceiling.hit:
+        velocity[1] = 0
+    return velocity
