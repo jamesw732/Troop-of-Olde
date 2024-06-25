0cf8bc5a81bfe1b0d38615dab0459a83fd2a91a8
jamesw732 <jamesmw732@gmail.com>
2024-06-25 19:39:38 -0400

    Improved stats window layout, show max for capped ratings

--- source/ui/stats_window.py
+++ source/ui/stats_window.py
@@ -27,7 +27,7 @@ class StatsWindow(Entity):
             ('speed', 'Speed'),
             ('armor', 'Armor')
         ]
-        self.stat_labels = [
+        self.innate_labels = [
             ('bdy', 'Body'),
             ('str', 'Strength'),
             ('dex', 'Dexterity'),
@@ -40,7 +40,7 @@ class StatsWindow(Entity):
 
         ]
         self.write_ratings()
-        self.write_stats()
+        self.write_innate()
         self.write_resists()
 
     def input(self, key):
@@ -58,45 +58,51 @@ class StatsWindow(Entity):
              position=(0.5, -0.5 * self.row_width, -1))
         start_row = 1
         end_row = start_row + math.ceil(len(self.rating_labels) / 2)
-        print(start_row, end_row)
         # Rows then columns
         locations = [
             [(self.col_width * (j + 0.5) + 0.01,
               -self.row_width * i - 0.5 * self.row_width, -2)
             for i in range(start_row, end_row)] for j in range(2)
         ]
+        # Flatten locations, and only take the number needed
         locations = list(itertools.chain(*locations))[:len(self.rating_labels)]
         for i, loc in enumerate(locations):
-            txt = f"{self.rating_labels[i][1]}: " \
-                  f"{getattr(self.player, self.rating_labels[i][0])}"
+            label = self.rating_labels[i][1]
+            attr = self.rating_labels[i][0]
+            cur = getattr(self.player, attr)
+            if attr in {"health", "mana", "stamina", "spellshield"}:
+                cap = getattr(self.player, "max" + attr)
+                txt = f"{label}: {cur}/{cap}"
+            else:
+                txt = f"{label}: {cur}"
             Text(text=txt, parent=self, position=loc, origin=(0, 0),
-                 world_scale=(14, 14), color=color.white)
+                 world_scale=(12, 12), color=color.white)
 
-    def write_stats(self):
-        Text(text="Innate Stats", parent=self, origin=(0, 0), world_scale=(18, 18),
+    def write_innate(self):
+        Text(text="Innate", parent=self, origin=(0, 0), world_scale=(18, 18),
              position=(0.5, (-5 - 0.5) * self.row_width, -2))
         start_row = 2 + math.ceil(len(self.rating_labels) / 2)
-        end_row = start_row + math.ceil(len(self.stat_labels) / 2)
-        print(start_row, end_row)
+        end_row = start_row + math.ceil(len(self.innate_labels) / 2)
         locations = [
             [(self.col_width * (j + 0.5) + 0.01,
               -self.row_width * i - 0.5 * self.row_width, -2)
             for i in range(start_row, end_row)] for j in range(2)
         ]
-        locations = list(itertools.chain(*locations))[:len(self.stat_labels)]
+        locations = list(itertools.chain(*locations))[:len(self.innate_labels)]
         for i, loc in enumerate(locations):
-            txt = f"{self.stat_labels[i][1]}: " \
-                  f"{getattr(self.player, self.stat_labels[i][0])}"
+            label = self.innate_labels[i][1]
+            attr = self.innate_labels[i][0]
+            cur = getattr(self.player, attr)
+            txt = txt = f"{label}: {cur}"
             Text(text=txt, parent=self, position=loc, origin=(0, 0),
-                 world_scale=(14, 14), color=color.white)
+                 world_scale=(12, 12), color=color.white)
 
     def write_resists(self):
         Text(text="Resists", parent=self, origin=(0, 0), world_scale=(18, 18),
              position=(0.5, (-9 - 0.5) * self.row_width, -2))
         start_row = 3 + math.ceil(len(self.rating_labels) / 2) \
-                      + math.ceil(len(self.stat_labels) / 2)
+                      + math.ceil(len(self.innate_labels) / 2)
         end_row = start_row + math.ceil(len(self.resist_labels) / 2)
-        print(start_row, end_row)
         # Rows then columns
         locations = [
             [(self.col_width * (j + 0.5) + 0.01,
@@ -105,10 +111,12 @@ class StatsWindow(Entity):
         ]
         locations = list(itertools.chain(*locations))[:len(self.resist_labels)]
         for i, loc in enumerate(locations):
-            txt = f"{self.resist_labels[i][1]}: " \
-                  f"{getattr(self.player, self.resist_labels[i][0])}"
+            label = self.resist_labels[i][1]
+            attr = self.resist_labels[i][0]
+            cur = getattr(self.player, attr)
+            txt = txt = f"{label}: {cur}"
             Text(text=txt, parent=self, position=loc, origin=(0, 0),
-                 world_scale=(14, 14), color=color.white)
+                 world_scale=(12, 12), color=color.white)
 
 
 # class RatingsSection(Entity):
