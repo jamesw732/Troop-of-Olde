23fea41dee8af32a6a061cd21c6493ef06c8b96a
jamesw732 <jamesmw732@gmail.com>
2024-06-25 18:12:09 -0400

    Made stats window generic with length of stats

--- source/ui/stats_window.py
+++ source/ui/stats_window.py
@@ -55,13 +55,17 @@ class StatsWindow(Entity):
     def write_ratings(self):
         Text(text="Ratings", parent=self, origin=(0, 0), world_scale=(18, 18),
              position=(0.5, -0.5 * self.row_width, -1))
+        start_row = 1
+        end_row = start_row + math.ceil(len(self.rating_labels) / 2)
+        print(start_row, end_row)
         # Rows then columns
         locations = [
             [(self.col_width * (j + 0.5) + 0.01,
               -self.row_width * i - 0.5 * self.row_width, -2)
-            for i in range(1, 5 - j)] for j in range(2)
+            for i in range(start_row, end_row)] for j in range(2)
         ]
-        for i, loc in enumerate(itertools.chain(*locations)):
+        locations = list(itertools.chain(*locations))[:len(self.rating_labels)]
+        for i, loc in enumerate(locations):
             txt = f"{self.rating_labels[i][1]}: " \
                   f"{getattr(self.player, self.rating_labels[i][0])}"
             Text(text=txt, parent=self, position=loc, origin=(0, 0),
@@ -70,12 +74,16 @@ class StatsWindow(Entity):
     def write_stats(self):
         Text(text="Innate Stats", parent=self, origin=(0, 0), world_scale=(18, 18),
              position=(0.5, (-5 - 0.5) * self.row_width, -2))
+        start_row = 2 + math.ceil(len(self.rating_labels) / 2)
+        end_row = start_row + math.ceil(len(self.stat_labels) / 2)
+        print(start_row, end_row)
         locations = [
             [(self.col_width * (j + 0.5) + 0.01,
               -self.row_width * i - 0.5 * self.row_width, -2)
-            for i in range(6, 9)] for j in range(2)
+            for i in range(start_row, end_row)] for j in range(2)
         ]
-        for i, loc in enumerate(itertools.chain(*locations)):
+        locations = list(itertools.chain(*locations))[:len(self.stat_labels)]
+        for i, loc in enumerate(locations):
             txt = f"{self.stat_labels[i][1]}: " \
                   f"{getattr(self.player, self.stat_labels[i][0])}"
             Text(text=txt, parent=self, position=loc, origin=(0, 0),
@@ -84,13 +92,18 @@ class StatsWindow(Entity):
     def write_resists(self):
         Text(text="Resists", parent=self, origin=(0, 0), world_scale=(18, 18),
              position=(0.5, (-9 - 0.5) * self.row_width, -2))
+        start_row = 3 + math.ceil(len(self.rating_labels) / 2) \
+                      + math.ceil(len(self.stat_labels) / 2)
+        end_row = start_row + math.ceil(len(self.resist_labels) / 2)
+        print(start_row, end_row)
         # Rows then columns
         locations = [
             [(self.col_width * (j + 0.5) + 0.01,
               -self.row_width * i - 0.5 * self.row_width, -2)
-            for i in range(10, 14 - j)] for j in range(2)
+            for i in range(start_row, end_row)] for j in range(2)
         ]
-        for i, loc in enumerate(itertools.chain(*locations)):
+        locations = list(itertools.chain(*locations))[:len(self.resist_labels)]
+        for i, loc in enumerate(locations):
             txt = f"{self.resist_labels[i][1]}: " \
                   f"{getattr(self.player, self.resist_labels[i][0])}"
             Text(text=txt, parent=self, position=loc, origin=(0, 0),
