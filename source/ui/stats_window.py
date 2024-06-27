53b843a067ef204a0a766326527180528eaf38ea
jamesw732 <jamesmw732@gmail.com>
2024-06-26 23:19:16 -0400

    Fixed major performance hit from stats window

--- source/ui/stats_window.py
+++ source/ui/stats_window.py
@@ -57,11 +57,17 @@ class StatsWindow(Entity):
         self.write_resists()
         self.write_mods()
 
+        self.update_rate = 1.0
+        self.update_timer = 0.0
+
     def update(self):
         # If this causes performance problems, just add a timer slower than every frame
         if self.visible:
-            for attr in self.entries:
-                self.update_label(attr)
+            self.update_timer += time.dt
+            if self.update_timer > self.update_rate:
+                self.update_timer -= self.update_rate
+                for attr in self.entries:
+                    self.update_label(attr)
 
     def write_ratings(self):
         Text(text="Ratings", parent=self, origin=(0, 0), world_scale=(18, 18),
