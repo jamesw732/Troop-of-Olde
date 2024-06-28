4fde970ae56957b745d3c96add0f6f87375089bd
jamesw732 <jamesmw732@gmail.com>
2024-06-27 22:29:55 -0400

    Added color option to ui.grid

--- source/ui/base.py
+++ source/ui/base.py
@@ -8,27 +8,27 @@ window_fg_color = color.hex("666666")
 
 
 
-def grid(entity, num_rows, num_cols, margin_x=0, margin_y=0):
+def grid(entity, num_rows, num_cols, margin_x=0, margin_y=0, color=color.white):
     """Helper function for drawing a grid over a UI element
     Margins are numbers from 0 to 1."""
     for row in range(num_rows + 1):
         vertices = [(margin_x,
                      -margin_y - (row / num_rows) * (1 - 2 * margin_y),
-                     0),
+                     -10),
 
                     (1 - margin_x,
                      -margin_y - (row / num_rows) * (1 - 2 * margin_y),
-                    0)]
-        Entity(parent=entity, model=Mesh(vertices=vertices, mode="line"), color=color.white)
+                    -10)]
+        Entity(parent=entity, model=Mesh(vertices=vertices, mode="line"), color=color)
     for col in range(num_cols + 1):
         vertices = [(margin_x + (col / num_cols) * (1 - 2 * margin_x),
                      -margin_y,
-                     0),
+                     -10),
 
                      (margin_x + col / num_cols * (1 - 2 * margin_x),
                       -1 + margin_y,
-                      0)]
-        Entity(parent=entity, model=Mesh(vertices=vertices, mode="line"), color=color.white)
+                      -10)]
+        Entity(parent=entity, model=Mesh(vertices=vertices, mode="line"), color=color)
 
 def get_hovered(entity):
     # Returns whether this entity is hovered, or whether any of its children are hovered
