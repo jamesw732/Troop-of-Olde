3a5bc90bcd2420d3af2ba7af98323ecf4450a219
jamesw732 <jamesmw732@gmail.com>
2024-06-30 10:52:35 -0400

    Added main for source/ui; previous few changes won't work without this

---
+++ source/ui/main.py
@@ -0,0 +1,7 @@
+from .bars import BarWindow
+from .player_window import PlayerWindow
+from .game_window import GameWindow
+
+bars = BarWindow()
+player_window = PlayerWindow()
+game_window = GameWindow()
