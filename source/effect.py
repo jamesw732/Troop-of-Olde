64e054282bd4cfe46f73d5c11b59fbf5c26ad080
jamesw732 <jamesmw732@gmail.com>
2024-12-23 12:32:05 -0500

    add missing files from repo

---
+++ source/effect.py
@@ -0,0 +1,31 @@
+from ursina import *
+import os
+import json
+from .ui.base import ui
+
+
+effects_path = os.path.join(os.path.dirname(__file__), "..", "data", "effects.json")
+with open(effects_path) as effects_json:
+    id_to_effect_data = json.load(effects_json)
+
+
+class Effect(Entity):
+    def __init__(self, effect_id):
+        super().__init__()
+        effect_id = str(effect_id)
+        effect_data = id_to_effect_data[effect_id]
+        for k, v in effect_data.items():
+            setattr(self, k, v)
+
+        if self.effect_type == "persistent":
+            self.timer = 0
+
+    def apply_to_char(self, char):
+        if self.effect_type == "instant":
+            if "damage" in self.effects:
+                dmg = self.effects["damage"]
+                char.health -= dmg
+                ui.gamewindow.add_message(f"{char.cname} is damaged for {dmg} damage!")
+            destroy(self)
+
+        
---
+++ source/power.py
@@ -0,0 +1,31 @@
+from ursina import *
+import os
+import json
+from .effect import Effect
+
+power_path = os.path.join(os.path.dirname(__file__), "..", "data", "powers.json")
+with open(power_path) as power_json:
+    id_to_power_data = json.load(power_json)
+
+
+class Power(Entity):
+    def __init__(self, power_id, char):
+        super().__init__()
+        power_id = str(power_id)
+        power_data = id_to_power_data[power_id]
+        for k, v in power_data.items():
+            setattr(self, k, v)
+
+        self.char = char
+
+        # set to True immediately after use
+        self.on_cooldown = False
+        # ticks up if self.on_cooldown
+        self.timer = 0
+
+    def apply_effect(self):
+        if self.target == "single":
+            if not self.char.target:
+                return
+            effect = Effect(self.effect_id)
+            effect.apply_to_char(self.char.target)
