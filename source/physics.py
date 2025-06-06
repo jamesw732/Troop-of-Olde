e6077593cc998f28b91c3f8415a4d5d33954bb06
jamesw732 <jamesmw732@gmail.com>
2025-06-06 00:17:27 -0400

    improve physics logic

--- source/physics.py
+++ source/physics.py
@@ -2,7 +2,7 @@
 from ursina import *
 import numpy
 
-from .base import PHYSICS_UPDATE_RATE
+from .base import PHYSICS_UPDATE_RATE, sqnorm
 from .gamestate import gs
 
 dt = PHYSICS_UPDATE_RATE
@@ -13,7 +13,7 @@ def set_gravity_vel(char):
     grav = char.velocity_components.get("gravity", Vec3(0, 0, 0))
     if not char.grounded and not char.jumping:
         grav -= Vec3(0, 5, 0)
-    elif char.grounded:
+    else:
         grav = Vec3(0, 0, 0)
     char.velocity_components["gravity"] = grav
 
@@ -41,53 +41,39 @@ def get_displacement(char):
 # PRIVATE
 def apply_physics(char, displacement):
     """Takes a character displacement and applies physics functions to ensure it can be applied"""
-    if displacement[1] <= 0:
-        handle_grounding(char, displacement)
-    displacement = handle_collision(char, displacement)
-    displacement = handle_upward_collision(char, displacement)
+    if not char.grounded:
+        displacement = handle_ungrounded_collision(char, displacement)
+        displacement = handle_upward_collision(char, displacement)
+    else:
+        displacement = handle_grounded_collision(char, displacement)
     return displacement
 
-def handle_grounding(char, displacement):
-    """Determine whether character is on the ground or not. Kills y displacement if grounded."""
-    ignore_traverse = char.ignore_traverse
-    # This doesn't seem to provide any sort of performance improvement
-    # if gs.ui:
-    #     ignore_traverse += gs.ui.colliders
-    ground = raycast(char.world_position + (0, char.height, 0), direction=char.down,
-                     ignore=ignore_traverse)
-    if not ground.hit:
-        char.grounded = False
-        return
-    # If ground is within next timestep, we're grounded.
-    if ground.distance <= char.height + max(0.01, abs(displacement[1])):
-        # print("Grounding")
+def handle_ungrounded_collision(char, displacement):
+    """Determine whether character's displacement will result in collision.
+
+    Assumes char.grounded is False
+    Adjusts and returns displacement that would cause character to not clip through floor"""
+    ray = raycast(char.world_position, direction=displacement, ignore=char.ignore_traverse)
+    if ray.hit and ray.distance ** 2 <= sqnorm(displacement):
+        displacement = ray.world_point - char.world_position + Vec3(0, 1e-3, 0)
         char.grounded = True
-        char.world_y = ground.world_point[1] + 1e-5
-        displacement[1] = 0
-    else:
-        char.grounded = False
+    return displacement
 
-def handle_collision(char, displacement, depth=0):
-    """Handles feet collision logic.
+def handle_grounded_collision(char, displacement):
+    """Handle collision logic when grounded
 
-    If displacement hits a surface, "project" displacement along that surface, then recursively
-    check again."""
-    # It's concave, don't try to handle it, just stop
-    if depth > 3:
-        return Vec3(0, 0, 0)
-    dist = distance((0, 0, 0), displacement)
-    collision_check = raycast(char.position, direction=displacement, distance=dist, ignore=char.ignore_traverse)
-    if collision_check.hit:
-        normal = collision_check.world_normal
-        speed = distance(Vec3.zero, displacement)
-        # It's a wall
-        if abs(normal.normalized()[1]) <= 0.4:
-            # Do normal projection formula
-            new_displacement = displacement - (numpy.dot(normal, displacement)) * normal
-            new_displacement[1] = 0
-        # It's a slope
+    Assumes char.grounded is True"""
+    down_ray = raycast(char.world_position, direction=char.down, ignore=char.ignore_traverse)
+    ray = raycast(char.world_position, direction=displacement, ignore=char.ignore_traverse)
+    disp_norm = distance((0, 0, 0), displacement)
+    if ray.hit and ray.distance < disp_norm:
+        normal = ray.world_normal
+        if normal.normalized()[1] <= 0.2:
+            # Intersection of the plane ax + by + cz = 0 with y = 0
+            direction = Vec3(normal[2], 0, -normal[0]).normalized()
+            displacement = direction * disp_norm * numpy.dot(direction, displacement)
         else:
-            point1 = collision_check.world_point
+            point1 = ray.world_point
             plane_const = numpy.dot(normal, point1)
             # take new x and z coordinates as if the surface didn't exist
             x2 = displacement[0] + point1[0]
@@ -96,11 +82,10 @@ def handle_collision(char, displacement, depth=0):
             y2 = (plane_const - x2 * normal[0] - z2 * normal[2]) / normal[1]
             point2 = Vec3(x2, y2, z2)
             # new point is the right direction but wrong distance, re-scale
-            new_displacement = (point2 - point1).normalized() * speed
-        return handle_collision(char, new_displacement, depth + 1)
-    else:
-        return displacement
-
+            displacement = (point2 - point1).normalized() * disp_norm
+    elif not down_ray.hit or down_ray.distance > 0.2:
+        char.grounded = False
+    return displacement
 
 def handle_upward_collision(char, displacement):
     """Blocks upward movement when jumping into a ceiling"""
