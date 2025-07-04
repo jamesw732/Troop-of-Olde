ee1baa393dcc295a203e7f9ee0ec33dd04bc04cb
jamesw732 <jamesmw732@gmail.com>
2025-07-04 16:12:41 -0400

    transfer movement/rotation logic and queued powers to InputHandler

--- source/client/controllers.py
+++ source/client/controllers.py
@@ -14,8 +14,7 @@ from ..skills import *
 class PlayerController(Entity):
     """Controller for the player character.
 
-    Handles most player inputs, client-side physics and interpolation, network updates.
-    Future: handles animations.
+    Handles client-side physics and interpolation, network updates.
     """
     def __init__(self, character=None):
         assert not gs.network.peer.is_hosting()
@@ -29,7 +28,8 @@ class PlayerController(Entity):
                 position = Vec3(0, 0.75, 0)
             )
             self.namelabel = NameLabel(character)
-            self.fix_namelabel()
+            self.namelabel.parent = self.focus
+            self.namelabel.world_position = self.character.position + Vec3(0, self.character.height * 1.3, 0)
             # self.shadow = Entity(origin=(0, -0.5, 0), scale=self.character.scale, model='cube',
             #                      color=color.yellow, rotation=self.character.rotation,
             #                      position=self.character.position)
@@ -65,11 +65,6 @@ class PlayerController(Entity):
         # Amount of time it should take client to match server given no inputs
         self.offset_duration = 0.5
 
-    def fix_namelabel(self):
-        """Simplify player character's namelabel"""
-        self.namelabel.parent = self.focus
-        self.namelabel.world_position = self.character.position + Vec3(0, self.character.height * 1.3, 0)
-
     def update(self):
         char = self.character
         # Predict position by interpolating between physics ticks
@@ -78,12 +73,6 @@ class PlayerController(Entity):
         char.position = lerp(self.prev_pos, self.target_pos, pct)
         # Predict character y rotation, enforce camera x rotation
         char.rotation_y = lerp_angle(self.prev_rot, self.target_rot, pct)
-        updown = held_keys['rotate_up'] - held_keys['rotate_down']
-        self.focus.rotate(Vec3(updown * 100 * time.dt, 0, 0))
-        if held_keys['right mouse']:
-            mouse_rotation = self.get_mouse_rotation()
-            self.focus.rotation_x += mouse_rotation[0]
-            self.mouse_y_rotation += mouse_rotation[1]
         # Fix character rotation
         self.character.rotation_x = 0
         self.character.rotation_z = 0
@@ -99,40 +88,15 @@ class PlayerController(Entity):
             camera.z = -1 * min(self.camdistance, dist)
         else:
             camera.z = -1 * self.camdistance
-        # Client-side prediction for movement
-        # pct = self.predict_timer / self.offset_duration
-        # char.position += lerp(Vec3(0, 0, 0), self.pos_offset, pct)
-        # char.rotation_y += lerp_angle(0, self.rot_offset, pct)
-        # Performance: This probably doesn't need to happen every frame, just when we move.
         if char.get_on_gcd():
             char.tick_gcd()
-        elif char.next_power is not None and not char.next_power.on_cooldown:
-            # Client-side power queueing basically just waits to request to use the power
-            power = char.next_power
-            tgt = power.get_target()
-            power.use()
-            gs.network.peer.request_use_power(gs.network.server_connection, power.power_id)
 
     @every(PHYSICS_UPDATE_RATE)
     def tick_physics(self):
         """Process movement inputs and send request to move to server.
         Update predicted physical state and reset lerp timer."""
         char = self.character
-        # Keyboard Movement
-        fwdback = held_keys['move_forward'] - held_keys['move_backward']
-        strafe = held_keys['strafe_right'] - held_keys['strafe_left']
-        keyboard_direction = Vec2(strafe, fwdback)
-        # Keyboard Rotation
-        rightleft = held_keys['rotate_right'] - held_keys['rotate_left']
-
-        conn = gs.network.server_connection
-        gs.network.peer.request_move(conn, self.sequence_number, keyboard_direction,
-                                     rightleft, self.mouse_y_rotation)
-
         # Client-side prediction for movement/rotation
-        char_speed = get_speed_modifier(char.speed)
-        kb_vel = (char.right * strafe + char.forward * fwdback).normalized() * 10 * char_speed
-        char.velocity_components["keyboard"] = kb_vel
         set_gravity_vel(char)
         set_jump_vel(char)
         displacement = get_displacement(char)
@@ -141,11 +105,6 @@ class PlayerController(Entity):
         self.prev_pos = char.position
         self.target_pos = char.position + displacement
         self.sn_to_pos[self.sequence_number] = self.target_pos
-        # may need to mulyiply by math.cos(math.radians(self.focus.rotation_x)), 0)
-        rotation = rightleft * 100 * PHYSICS_UPDATE_RATE + self.mouse_y_rotation
-        self.prev_rot = self.target_rot
-        self.target_rot = char.rotation_y + rotation + self.rot_offset
-        self.sn_to_rot[self.sequence_number] = self.target_rot
         # Just some necessary bookkeeping things
         self.predict_timer = 0
         char.displacement_components["server_offset"] = Vec3(0, 0, 0)
@@ -153,13 +112,41 @@ class PlayerController(Entity):
         self.mouse_y_rotation = 0
         self.sequence_number += 1
 
-    def get_mouse_rotation(self):
-        """Handles rotation from right clicking and dragging the mouse."""
-        # Mouse rotation:
+    def update_keyboard_velocity(self, fwdback, strafe):
+        """Update velocity obtained from player inputs.
+
+        This velocity will be consumed in tick_physics."""
+        char = self.character
+        char_speed = get_speed_modifier(char.speed)
+        kb_vel = (char.right * strafe + char.forward * fwdback).normalized() * 10 * char_speed
+        char.velocity_components["keyboard"] = kb_vel
+
+    def update_target_rotation(self, rightleft_rot):
+        """Combines keyboard/mouse rotation factors to determine a target rotation"""
+        rotation = rightleft_rot * 100 * PHYSICS_UPDATE_RATE + self.mouse_y_rotation
+        self.prev_rot = self.target_rot
+        self.target_rot = self.character.rotation_y + rotation + self.rot_offset
+        self.sn_to_rot[self.sequence_number] = self.target_rot
+
+    def handle_updown_keyboard_rotation(self, updown):
+        """Handles up/down arrow key rotation.
+
+        Only affects up-down rotation, which is irrelevant to server.
+        Nothing sent to server as a result of this method."""
+        self.focus.rotate(Vec3(updown * 100 * time.dt, 0, 0))
+
+    def handle_mouse_rotation(self):
+        """Updates self.mouse_y_rotation according to mouse rotation.
+
+        Does not update the character's rotation directly, instead this
+        quantity is stored cumulatively over one physics tick period, then
+        used to determine the next target rotation."""
         offset = mouse.position - self.prev_mouse_position
         self.prev_mouse_position = mouse.position
         vel = Vec3(-1 * offset[1], offset[0] * math.cos(math.radians(self.focus.rotation_x)), 0)
-        return vel * 200
+        mouse_rotation = vel * 200
+        self.focus.rotation_x += mouse_rotation[0]
+        self.mouse_y_rotation += mouse_rotation[1]
 
     def input(self, key):
         """Updates from single client inputs"""
--- source/client/input_handler.py
+++ source/client/input_handler.py
@@ -1,13 +1,16 @@
 import json
 
-from ursina import Entity
+from ursina import Entity, held_keys, every, Vec2
 import ursina.input_handler
 
+from ..base import PHYSICS_UPDATE_RATE
 from ..gamestate import gs
 from ..networking import network
 
 class InputHandler(Entity):
     def __init__(self):
+        super().__init__()
+
         with open("data/key_mappings.json") as keymap:
             keymap_json = json.load(keymap)
             for k, v in keymap_json.items():
@@ -18,3 +21,51 @@ class InputHandler(Entity):
             if key == "c":
                 print("Attempting to connect")
                 network.peer.start("localhost", 8080, is_host=False)
+
+    def update(self):
+        ctrl = gs.playercontroller
+        if ctrl is None:
+            # Eventually, this class will depend on PlayerController and this check will
+            # be deferred to PlayerController as a Character existence check
+            return
+        char = ctrl.character
+        if char is None:
+            return
+        # Client-side movement/rotation updates
+        updown_rot = held_keys['rotate_up'] - held_keys['rotate_down']
+        ctrl.handle_updown_keyboard_rotation(updown_rot)
+        if held_keys["right mouse"]:
+            ctrl.handle_mouse_rotation()
+
+        # Power queueing is essentially a delayed input
+        if not char.get_on_gcd() and char.next_power is not None and not char.next_power.on_cooldown:
+            # Client-side power queueing basically just waits to request to use the power
+            power = char.next_power
+            tgt = power.get_target()
+            power.use()
+            gs.network.peer.request_use_power(gs.network.server_connection, power.power_id)
+
+    @every(PHYSICS_UPDATE_RATE)
+    def tick_movement(self):
+        ctrl = gs.playercontroller
+        if ctrl is None:
+            # Eventually, this class will depend on PlayerController and this check will
+            # be deferred to PlayerController as a Character existence check
+            return
+        char = ctrl.character
+        if char is None:
+            return
+        # Send movement/rotation inputs to server
+        # Keyboard Movement
+        fwdback = held_keys['move_forward'] - held_keys['move_backward']
+        strafe = held_keys['strafe_right'] - held_keys['strafe_left']
+        keyboard_direction = Vec2(strafe, fwdback)
+        # Keyboard Rotation
+        rightleft_rot = held_keys['rotate_right'] - held_keys['rotate_left']
+
+        conn = gs.network.server_connection
+        gs.network.peer.request_move(conn, ctrl.sequence_number, keyboard_direction,
+                                     rightleft_rot, ctrl.mouse_y_rotation)
+        # Predict movement/rotation updates client-side
+        ctrl.update_keyboard_velocity(fwdback, strafe)
+        ctrl.update_target_rotation(rightleft_rot)
