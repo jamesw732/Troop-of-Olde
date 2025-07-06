99088026d4c3702717d7f79b017022e25a09d8ff
jamesw732 <jamesmw732@gmail.com>
2025-07-05 23:24:09 -0400

    remove states subpackage, replace with states module

--- source/states.py
+++ source/states.py
@@ -1,9 +1,14 @@
+"""Contains API for States, including functions for building states."""
 import os
+import json
 
+from ursina import *
 from ursina import Vec3, Vec4, color, load_model, Entity, destroy
 from ursina.mesh_importer import imported_meshes
 
-from ..base import all_skills, default_equipment, default_phys_attrs, default_cb_attrs, models_path
+from .base import all_skills, data_path, default_equipment, default_phys_attrs, \
+        default_cb_attrs, models_path
+from .gamestate import gs
 
 class State(list):
     """Base class for all State types. Never meant to be initialized directly,
@@ -268,3 +273,56 @@ class SkillsState(State):
             v = reader.read(t)
             state[i] = v
         return state
+
+
+def get_player_states_from_data(pc_data, player_name):
+    """Returns the states necessary to request a Player Character
+    be loaded from server.
+    pc_data: npc data loaded from json and keyed by name
+    player_name: the name of the character
+    returns a list of states to use as arguments to request_enter_world"""
+    pstate_raw = pc_data.get("pstate", {})
+    pstate_raw["cname"] = player_name
+    basestate_raw = pc_data.get("basestate", {})
+    skills_raw = pc_data.get("skills", {})
+    equipment = pc_data.get("equipment", [])
+    inventory = pc_data.get("inventory", [])
+    powers = pc_data.get("powers", [])
+
+    pstate = PhysicalState(pstate_raw)
+    basestate = BaseCombatState(basestate_raw)
+    skills = SkillsState(skills_raw)
+
+    return pstate, basestate, equipment, inventory, skills, powers
+
+
+def get_npc_states_from_data(npc_data, npc_name):
+    """Return the states necessary to load an NPC from JSON data.
+    npc_data: npc data loaded from json and keyed by name
+    npc_name: the name of the character
+    returns a dict mapping ServerCharacter argument name to state"""
+    pstate_raw = npc_data.get("physical", {})
+    pstate_raw["cname"] = npc_name
+    basestate_raw = npc_data.get("basestate", {})
+
+    pstate = PhysicalState(pstate_raw)
+    basestate = BaseCombatState(basestate_raw)
+
+    return {"pstate": pstate, "cbstate": basestate}
+
+
+def container_to_ids(container, id_type="inst_id"):
+    """Convert a container (Character attribute) to one sendable over the network
+
+    container: list containing Items
+    id_type: the literal id attribute of the item, or tuple of id attributes
+    In the latter case, returns a 1d list stacked in order of the id attributes"""
+    if isinstance(id_type, tuple):
+        return [getattr(item, id_subtype) if hasattr(item, id_subtype) else -1 for id_subtype in id_type
+                for item in container]
+    return [getattr(item, id_type) if hasattr(item, id_type) else -1 for item in container]
+
+
+def ids_to_container(id_container):
+    """Convert container of inst ids to a list of objects"""
+    return [gs.network.inst_id_to_item.get(itemid, None) for itemid in id_container]
