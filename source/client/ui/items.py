04ca10c06f70b37374ec9a3b2930ec9739c3dd1d
jamesw732 <jamesmw732@gmail.com>
2025-07-31 22:36:19 -0400

    add item UI item movement logic to ItemsManager

--- source/client/ui/items.py
+++ source/client/ui/items.py
@@ -7,12 +7,13 @@ from ... import *
 
 class ItemFrame(Entity):
     """A rectangular grid storing items."""
-    def __init__(self, char, grid_size, container, slot_labels=[], **kwargs):
-        self.char = char
+    def __init__(self, grid_size, container, slot_labels=[],
+                 start_dragging=lambda x, y: None,**kwargs):
         self.grid_size = grid_size
         self.container = container
         # pad the slot labels with empty strings
         self.slot_labels = slot_labels + ([""] * (len(container) - len(slot_labels)))
+        self.start_dragging = start_dragging
 
         self.boxes = [None] * len(container)
 
@@ -32,20 +33,11 @@ class ItemFrame(Entity):
 
         grid(self, int(grid_size[1]), int(grid_size[0]), color=color.black)
 
-        self.dragging_icon = None
-        self.dragging_box = None
-        self.drag_threshold = 0.2
-        self.click_start_time = time.time()
-        self.step = Vec3(0, 0, 0)
-
     def on_click(self):
         hovered_slot = self.get_hovered_slot()
-        self.dragging_box = self.boxes[hovered_slot]
-        self.dragging_icon = self.dragging_box.icon
-        if self.dragging_icon is None:
-            return
-        self.step = self.dragging_box.get_position(camera.ui) - mouse.position
-        self.click_start_time = time.time()
+        dragging_box = self.boxes[hovered_slot]
+        dragging_icon = dragging_box.icon
+        self.start_dragging(dragging_icon, dragging_box)
 
     def get_hovered_slot(self):
         ui_mouse_pos = mouse.position * camera.ui.scale
@@ -54,41 +46,6 @@ class ItemFrame(Entity):
         slot = Vec2(rel_mouse_pos[0], rel_mouse_pos[1]) / slot_world_pos
         slot = (int(slot[0]), int(slot[1]))
         return slot[0] - slot[1] * int(self.grid_size[0])
-
-    def input(self, key):
-        if key == "left mouse up" and self.dragging_icon is not None:
-            # Item was being dragged and was just released
-            other_entity = mouse.hovered_entity
-            if not isinstance(other_entity, ItemFrame):
-                self.dragging_icon.position = Vec3(0, 0, -1)
-            else:
-                item = self.dragging_icon.item
-                my_slot = self.dragging_icon.parent.slot
-                hovered_slot = other_entity.get_hovered_slot()
-                drop_box = other_entity.boxes[hovered_slot]
-                # Clicked and released quickly without moving out of this box
-                if my_slot == hovered_slot and time.time() - self.click_start_time < self.drag_threshold:
-                    option = item.leftclick
-                    if option == "equip":
-                        char = self.char
-                        other_slot = self.char.find_auto_equip_slot(item)
-                        other_container = char.equipment
-                    if option == "unequip":
-                        char = self.char
-                        other_slot = self.char.find_auto_inventory_slot()
-                        other_container = char.inventory
-                # Clicked and released on another box
-                else:
-                    other_slot = hovered_slot
-                    other_container = drop_box.container
-                conn = network.server_connection
-                network.peer.request_move_item(conn, item.inst_id, other_container.name, other_slot)
-            self.dragging_icon = None
-            self.dragging_box = None
-
-    def update(self):
-        if self.dragging_icon is not None and mouse.position:
-            self.dragging_icon.set_position(mouse.position + self.step, camera.ui)
 
 
 class ItemBox(Entity):
--- source/client/ui/items_manager.py
+++ source/client/ui/items_manager.py
@@ -1,6 +1,7 @@
 from ursina import *
 
 from .items import *
+from ... import *
 
 
 class ItemsManager(Entity):
@@ -11,9 +12,16 @@ class ItemsManager(Entity):
         self.inst_id_to_item_icon = {}
         self.item_frames = {}
 
+        self.dragging_icon = None
+        self.dragging_box = None
+        self.drag_threshold = 0.2
+        self.click_start_time = time.time()
+        self.step = Vec3(0, 0, 0)
+
     def make_item_frame(self, grid_size, container, slot_labels=[], **kwargs):
         """Creates an ItemFrame and adds it to self.item_frames."""
-        frame = ItemFrame(self.char, grid_size, container, slot_labels=slot_labels, **kwargs)
+        frame = ItemFrame(grid_size, container, slot_labels=slot_labels,
+                          start_dragging=self.start_dragging_icon, **kwargs)
         self.item_frames[container.name] = frame
         for i, box in enumerate(frame.boxes):
             item = container[i]
@@ -41,4 +49,47 @@ class ItemsManager(Entity):
                     icon = self.inst_id_to_item_icon[item.inst_id]
                 box.set_item_icon(icon)
 
+    def start_dragging_icon(self, icon, box):
+        self.dragging_box = box
+        self.dragging_icon = icon
+        if self.dragging_icon is None:
+            return
+        self.step = self.dragging_box.get_position(camera.ui) - mouse.position
+        self.click_start_time = time.time()
+
+    def input(self, key):
+        if key == "left mouse up" and self.dragging_icon is not None:
+            # Item was being dragged and was just released
+            other_entity = mouse.hovered_entity
+            if not isinstance(other_entity, ItemFrame):
+                self.dragging_icon.position = Vec3(0, 0, -1)
+            else:
+                item = self.dragging_icon.item
+                my_slot = self.dragging_icon.parent.slot
+                hovered_slot = other_entity.get_hovered_slot()
+                drop_box = other_entity.boxes[hovered_slot]
+                # Clicked and released quickly without moving out of this box
+                if my_slot == hovered_slot and \
+                        time.time() - self.click_start_time < self.drag_threshold:
+                    option = item.leftclick
+                    if option == "equip":
+                        char = self.char
+                        other_slot = self.char.find_auto_equip_slot(item)
+                        other_container = char.equipment
+                    if option == "unequip":
+                        char = self.char
+                        other_slot = self.char.find_auto_inventory_slot()
+                        other_container = char.inventory
+                # Clicked and released on another box
+                else:
+                    other_slot = hovered_slot
+                    other_container = drop_box.container
+                conn = network.server_connection
+                network.peer.request_move_item(conn, item.inst_id, other_container.name, other_slot)
+            self.dragging_icon = None
+            self.dragging_box = None
+
+    def update(self):
+        if self.dragging_icon is not None and mouse.position:
+            self.dragging_icon.set_position(mouse.position + self.step, camera.ui)
 
